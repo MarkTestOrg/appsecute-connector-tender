e18e153d4676957297e55a78f1bfa25eaa09d3d9
Mark Cox <mark@appsecute.com>
2012-12-20 11:19:06 +1300

    Beginnings of OAuth support

--- models/oauthclient.js
+++ models/oauthclient.js
@@ -1,7 +1,15 @@
 /**
- * Created with JetBrains WebStorm.
- * User: Mark
- * Date: 19/12/12
- * Time: 4:25 PM
- * To change this template use File | Settings | File Templates.
+ * A model representing a Client application that wants to talk OAuth to our server (e.g. Appsecute itself)
+ * @type {*}
  */
+
+var mongoose = require('mongoose');
+var database = require('../lib/database/database.js');
+
+var oauthClientSchema = new mongoose.Schema({
+    // TODO: Add fields for client, see oauth2orize example for ideas
+});
+
+var oauthClientModel = database.model('OAuthClient', oauthClientSchema);
+
+module.exports = oauthClientModel;
--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var express = require('express');
 var app = express();
 var port = 3002;
 
+// Set up passport for authentication
+var passport = require('passport');
+
+// Set up an OAuth2 Server
+var oauth2orize = require('')
+    , oauth2 = require('./lib/oauth/oauth2')
+var oAuthServer = oauth2orize.createServer();
+
 // Configure the environment
 require('./config.js')(app, express);
 
