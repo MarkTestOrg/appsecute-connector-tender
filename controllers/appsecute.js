baa7a6b474c5ba464e521467df32f1d753d99b15
Mark Cox <mark@appsecute.com>
2013-01-09 21:30:14 +1300

    Added start of tender client, fetches categories

--- controllers/appsecute.js
+++ controllers/appsecute.js
@@ -6,6 +6,7 @@
 var _ = require('underscore');
 var siteMappingModel = require('../models/sitemapping.js');
 var passport = require('passport');
+var tenderClient = require('../lib/tender/tenderclient.js');
 
 
 module.exports = function (app) {
@@ -19,15 +20,15 @@ module.exports = function (app) {
 
 
     /**
-     * Formats a GitHub repo as an Appsecute component.
-     * @param {Object} repo The GitHub repo.
+     * Formats a Tender Support category repo as an Appsecute component.
+     * @param {Object} category The Tender Support category, in the format returned by their API
      * @return {Object} A component ready to be sent to Appsecute.
      */
-    var componentFromRepo = function (repo) {
+    var componentFromTenderCategory = function (category) {
         return {
-            id:encodeURIComponent(repo.full_name),
-            name:repo.name,
-            description:repo.description
+            name:category.name,
+            id:category.permalink, // this is the 'sanitised name' from tender
+            description:category.summary
         }
     };
 
@@ -36,27 +37,44 @@ module.exports = function (app) {
      * Appsecute calls this to get a listing of components for the current user.
      */
     app.get('/appsecute/components',
-        passport.authenticate('bearer', { session: false }),
+        passport.authenticate('bearer', { session:false }),
 
-        function (req, res) {
+        function (req, res, next) {
             var username = req.user.username;
 
-            res.send(
-                {
-                    'Tender Components':
-                    [
-                        {
-                            name: "Test response for user " + username,
-                            id: 'test1',
-                            description: 'This is a test description'
-                        },
-                        {
-                            name: "A second item",
-                            id: 'test2',
-                            description: 'This is a second test description'
-                        }
-                    ]
-            });
+            // The API key is associated with the user who authenticated when Appsecute was issued an OAuth access token
+            // When Appsecute's token is redeemed passport looks up the original user as the content to run under
+            var apikey = req.user.apikey;
+
+            // TODO: Allow user to specify the tender site name
+            // TODO: Allow user to specify multiple tender sites and store them in the database
+            var tenderSite = 'appsecute';
+
+            if (apikey) {
+                // Use the API key to go get a list of components from Tender support
+                tenderClient.listCategories(tenderSite, apikey, function (err, categories) {
+                    if (!err) {
+                        var components = {};
+
+                        // TODO: Support multiple tender sites
+                        components[tenderSite] = [];
+
+                        _.each(categories, function (category) {
+                            components[tenderSite].push(componentFromTenderCategory(category));
+                        });
+
+                        res.send(components);
+                    }
+                    else {
+                        next(err); // Error listing categories from Tender
+                    }
+                });
+            }
+            else {
+                // We don't have an API key associated with Appsecute's OAuth access token
+                // Send a 401 to tell Appsecute to re-do OAuth authentication
+                res.send(401, "API key not available");
+            }
         }
     );
 
