f012805954329633ba2ea93be954f6c95d702369
Mark Cox <mark@appsecute.com>
2013-01-22 12:28:54 +1300

    Removed OAuth server from tender connector

--- auth.js
+++ auth.js
@@ -2,35 +2,12 @@
  * Module dependencies.
  */
 var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy
-  , BasicStrategy = require('passport-http').BasicStrategy
   , AppsecuteStrategy = require('./lib/passport-appsecute/passport-appsecute').Strategy
-  , ClientPasswordStrategy = require('passport-oauth2-client-password').Strategy
   , BearerStrategy = require('passport-http-bearer').Strategy
-  , db = require('./lib/db')
-
-
 
 var appsecuteConnectorApi = require('appsecute-connector-api');
 
-
-/**
- * LocalStrategy
- *
- * This strategy is used to authenticate users based on a username and password.
- * Anytime a request is made to authorize an application, we must ensure that
- * a user is logged in before asking them to approve the request
- */
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (user.password != password) { return done(null, false); }
-      return done(null, user);
-    });
-  }
-));
+var db = require('./lib/db');
 
 
 /**
@@ -71,48 +48,16 @@ passport.deserializeUser(function(id, done) {
 
 
 /**
- * BasicStrategy & ClientPasswordStrategy
- *
- * These strategies are used to authenticate registered OAuth clients.  They are
- * employed to protect the `token` endpoint, which consumers use to obtain
- * access tokens.  The OAuth 2.0 specification suggests that clients use the
- * HTTP Basic scheme to authenticate.  Use of the client password strategy
- * allows clients to send the same credentials in the request body (as opposed
- * to the `Authorization` header).  While this approach is not recommended by
- * the specification, in practice it is quite common.
- */
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    db.clients.findByClientId(username, function(err, client) {
-      if (err) { return done(err); }
-      if (!client) { return done(null, false); }
-      if (client.clientSecret != password) { return done(null, false); }
-      return done(null, client);
-    });
-  }
-));
-
-passport.use(new ClientPasswordStrategy(
-  function(clientId, clientSecret, done) {
-    db.clients.findByClientId(clientId, function(err, client) {
-      if (err) { return done(err); }
-      if (!client) { return done(null, false); }
-      if (client.clientSecret != clientSecret) { return done(null, false); }
-      return done(null, client);
-    });
-  }
-));
-
-/**
  * BearerStrategy
  *
- * This strategy is used to authenticate users based on an access token (aka a
- * bearer token).  The user must have previously authorized a client
- * application, which is issued an access token to make requests on behalf of
- * the authorizing user.
+ * This strategy is used to authenticate client applications who signed in using OAuth.
+ * Normally this would be Appsecute wanting to take some sort of action through an API.
+ * users based on an access token (aka a bearer token).
+ * The user must have previously authorized a client application with the authorization server,
+ * in this case Appsecute's OAuth2 server.
  */
 passport.use(new BearerStrategy(function(accessToken, done) {
-    // Try looking up the access token in our local store
+    // Try looking up the access token in our local cache
     db.accessTokens.find(accessToken, function(err, token) {
         if (err) { return done(err); }
         if (token) {
--- server.js
+++ server.js
@@ -7,14 +7,10 @@ var express = require('express');
 var app = express();
 var port = 3002;
 
-// Set up an OAuth2 Server
-var oauth2 = require('./lib/oauth/oauth2');
-
 // Configure the environment
 require('./config.js')(app, express);
 
 // Load our controllers
-require('./controllers/oauthsite.js')(app, oauth2);
 require('./controllers/appsecute.js')(app);
 require('./controllers/tender.js')(app);
 require('./controllers/oauth2-client.js')(app);
@@ -22,26 +18,3 @@ require('./controllers/oauth2-client.js')(app);
 module.exports = app.listen(port);
 
 console.log("Tender Support connector listening on port %d", port);
-
-
-
-
-//app.engine('.ejs', require('ejs').__express);
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
-// Dummy users
-var users = [
-    { name: 'tobi', email: 'tobi@learnboost.com' },
-    { name: 'loki', email: 'loki@learnboost.com' },
-    { name: 'jane', email: 'jane@learnboost.com' }
-];
-
-app.get('/', function(req, res){
-    console.log("About to render test out of " + app.get('views'));
-    res.render('test', {
-        users: users,
-        title: "EJS example",
-        header: "Some users"
-    });
-});
